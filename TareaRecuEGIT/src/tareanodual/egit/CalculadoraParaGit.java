f3d2dff474c4d23312e44c0374f3c766d3867b5d
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2024-04-10 02:58:42 +0200

    Se añade multiplicaciom y Division

--- TareaRecuEGIT/src/tareanodual/egit/CalculadoraParaGit.java
+++ TareaRecuEGIT/src/tareanodual/egit/CalculadoraParaGit.java
@@ -13,6 +13,14 @@ public class CalculadoraParaGit {
 		return (op1 - op2);
 		
 	}
+	public  double multiplicacion(double op1, double op2) {
+		return (op1 * op2);
+		
+	}
+	public  double division(double op1, double op2) {
+		return (op1 / op2);
+		
+	}
 
 	public static void main(String[] args) {
 	
