d650bc1b4776b8e8cc3c5da425820d9c6c91c585
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2024-04-10 02:48:11 +0200

    Version primera de la calculadora

---
+++ TareaRecuEGIT/src/tareanodual/egit/CalculadoraParaGit.java
@@ -0,0 +1,22 @@
+package tareanodual.egit;
+
+public class CalculadoraParaGit {
+
+	public CalculadoraParaGit() {
+		
+	}
+	public  double suma(double op1, double op2) {
+		return (op1 + op2);
+		
+	}
+	public  double resta(double op1, double op2) {
+		return (op1 - op2);
+		
+	}
+
+	public static void main(String[] args) {
+	
+		CalculadoraParaGit calc = new CalculadoraParaGit();
+		System.out.println(calc.suma(225, 25));
+}
+	}
